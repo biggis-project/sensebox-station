dea0cc65f1c07d1b3e2f115997b4ee729ab0b99d
Jochen Lutz <jlu@gmx.de>
2016-11-30 13:20:39 +0100

    in Slick schema, column "location" is now nullable

--- FlinkDbSink/src/main/scala/Tables.scala
+++ FlinkDbSink/src/main/scala/Tables.scala
@@ -4,17 +4,17 @@ import slick.driver.PostgresDriver.api._
 import slick.lifted.{ForeignKeyQuery, ProvenShape}
 
 class Measurements(tag: Tag)
-  extends Table[(String, String, Timestamp, Double, String)](tag, "measurements") {
+  extends Table[(String, String, Timestamp, Double, Option[String])](tag, "measurements") {
 
   // this does not follow slick standards: using lower case database labels because Postgres requires quotes on uppercase labels
   def boxid: Rep[String] = column[String]("boxid")
   def sensorid: Rep[String] = column[String]("sensorid")
   def ts: Rep[Timestamp] = column[Timestamp]("ts")
   def value: Rep[Double] = column[Double]("value")
-  def location: Rep[String] = column[String]("location") //TODO: sinnvollerer Typ (PostGIS?) //TODO: may NULL
+  def location: Rep[Option[String]] = column[Option[String]]("location") //TODO: sinnvollerer Typ (PostGIS?)
 
   // Every table needs a * projection with the same type as the table's type parameter
-  def * : ProvenShape[(String, String, Timestamp, Double, String)] =
+  def * : ProvenShape[(String, String, Timestamp, Double, Option[String])] =
   (boxid, sensorid, ts, value, location)
 
   //TODO: pkey(boxid,sensorid,ts)
