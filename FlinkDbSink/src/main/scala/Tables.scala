c67a6c4eedf5af3e3790e43a628ae56aac3982e1
Jochen Lutz <jlu@gmx.de>
2016-12-01 11:39:29 +0100

    Table wrapper Measurements now with explicit SqlType (for MySQL)

--- FlinkDbSink/src/main/scala/Tables.scala
+++ FlinkDbSink/src/main/scala/Tables.scala
@@ -7,11 +7,11 @@ class Measurements(tag: Tag)
   extends Table[(String, String, Timestamp, Double, Option[String])](tag, "measurements") {
 
   // this does not follow slick standards: using lower case database labels because Postgres requires quotes on uppercase labels
-  def boxid: Rep[String] = column[String]("boxid")
-  def sensorid: Rep[String] = column[String]("sensorid")
+  def boxid: Rep[String] = column[String]("boxid",O.SqlType("varchar(100)"))
+  def sensorid: Rep[String] = column[String]("sensorid",O.SqlType("varchar(100)"))
   def ts: Rep[Timestamp] = column[Timestamp]("ts")
   def value: Rep[Double] = column[Double]("value")
-  def location: Rep[Option[String]] = column[Option[String]]("location") //TODO: sinnvollerer Typ (PostGIS?)
+  def location: Rep[Option[String]] = column[Option[String]]("location",O.SqlType("varchar(100)")) //TODO: sinnvollerer Typ (PostGIS?)
 
   // Every table needs a * projection with the same type as the table's type parameter
   def * : ProvenShape[(String, String, Timestamp, Double, Option[String])] =
